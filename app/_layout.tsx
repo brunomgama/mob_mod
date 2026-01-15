7be8559c14b603fa2b0375a43171c8360024887f
Bruno Gama <bmogama@gmail.com>
2026-01-15 15:49:39 +0100

    NativeWind and Tailwind installed and configured

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-2xl font-bold">
+        Hello from NativeWind!
+      </Text>
+    </View>
+  );
 }
